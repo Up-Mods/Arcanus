f673322fd5e7356c290db1b376b840ab22ede62f
CammiePone <inazuma261@gmail.com>
2025-01-15 23:37:17 -0800

    Revert "removed bad import because intellij dummy"
    This reverts commit 3f74cb03df8d89c221c97a6d96f3a6d4f456a837.

--- Common/src/main/java/dev/cammiescorner/arcanus/api/entities/Targetable.java
+++ Common/src/main/java/dev/cammiescorner/arcanus/api/entities/Targetable.java
@@ -1,6 +1,8 @@
 package dev.cammiescorner.arcanus.api.entities;
 
+import dev.cammiescorner.arcanus.common.MainDuck;
 import dev.cammiescorner.arcanus.common.MainHelper;
+import dev.cammiescorner.arcanus.common.util.PlayerHelper;
 import net.minecraft.world.entity.Entity;
 
 public interface Targetable {
